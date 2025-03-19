524f216676b027be42372c8c7373b6e755541172
thepablosantos <iphonepablosantos@gmail.com>
2025-03-19 20:35:22 +0000

    Add view and pure functions to SimpleStorage contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -9,4 +9,12 @@ function storeNumber(uint256 _number) public {
     storedNumber = _number;
 }
 
+function retrieveNumber() public view returns (uint256) {
+    return storedNumber;
+}
+
+function addNumbers(uint256 a, uint256 b) public pure returns (uint256) {
+    return a + b;
+}
+
 }
